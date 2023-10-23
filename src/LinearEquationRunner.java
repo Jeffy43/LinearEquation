a7151e1cc6a29cbad207a0fbdbe4061768ad0eb4
Jeffy <jeffyp@nyctudents.net>
2023-10-23 09:12:07 -0400

    Update

--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
 public class LinearEquationRunner {
+    public static void start(){
+        System.out.println("Welcome!");
+    }
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         //Parse for coordinates
-        System.out.println("Welcome!");
+        start();
         System.out.println("Enter coordinate 1: ");
         String a = scan.nextLine();
         double x1 = Double.parseDouble(a.substring(1,a.indexOf(",")));
@@ -12,15 +15,20 @@ public class LinearEquationRunner {
         String b = scan.nextLine();
         double x2 = Double.parseDouble(b.substring(1,b.indexOf(",")));
         double y2 = Double.parseDouble(b.substring(b.indexOf(",") + 1, b.length() - 1));
+        if (x1 == x2){
+            System.out.print("The line is a vertical. The equation of the line: x = " + x1);
+            System.exit(0);
+        }
         //Constructor
         LinearEquation equation = new LinearEquation(x1, y1, x2, y2);
-        //Utilizes Method
+        //Utilizes methods and prints info out
         System.out.println("----- Line info -----");
         System.out.println(equation.lineInfo());
         System.out.println();
         System.out.println("Coordinate for x: ");
         double x = scan.nextDouble();
         System.out.println("The point on 1,the line is" + equation.coordinateForX(x));
+
 
     }
 }
